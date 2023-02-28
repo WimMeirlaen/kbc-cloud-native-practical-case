56ba85e09cf6870d4c8a10f5b2d372bacda27b59
WimMeirlaen <wim_meirlaen@hotmail.com>
2023-02-28 10:58:22 +0100

    lab-01 commit

---
+++ ezGroceriesShopppingList/src/main/java/com/ezgroceries/shoppinglist/EzGroceriesShopppingListApplication.java
@@ -0,0 +1,13 @@
+package com.ezgroceries.shoppinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzGroceriesShopppingListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzGroceriesShopppingListApplication.class, args);
+    }
+
+}
